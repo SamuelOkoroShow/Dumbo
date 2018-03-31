3b870aad66dd9dead2255482fe7bf1995376389b
Catherine Lu <catherinelu@utexas.edu>
2018-03-31 17:13:00 -0500

    wholesome hacks progress

--- Dumbo/background.js
+++ Dumbo/background.js
@@ -1,8 +1,9 @@
 var enabled = true;
 chrome.webRequest.onBeforeRequest.addListener(
 	function(details) {
+		console.log("blocking:", details.url);
 		return {cancel: enabled };
 	},
-	{urls: youtube_blocked_domains},
+	{urls: blocked_domains},
 	["blocking"]
 );
---
+++ Dumbo/content.js
@@ -0,0 +1,4 @@
+javascript:var images = document.getElementsByTagName('img');
+for (var i = 0, l = images.length; i < l; i++) {
+  images[i].src = 'https://thumbs.gfycat.com/PinkHonoredIzuthrush-size_restricted.gif';
+}
